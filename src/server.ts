7e5a5e4a0987e96589b0399301bdb3687557f66a
Renato Nascimento <renatocorreianascimento@gmail.com>
2025-10-02 15:15:39 -0300

    Preparaçao do modulo backend construçao de API para disponibilização das informações

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,23 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 import {
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-	origin: "http://localhost:5173",
-});
-
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
-
-app.get("/health", () => {
-	return "OK";
+  origin: 'http://localhost:5173',
 })
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => 'OK')
 
 app.register(getRoomsRoute)
 
