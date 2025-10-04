a9219f77c92bc4cc5923b93014ce5b74247d73f1
Renato Nascimento <renatocorreianascimento@gmail.com>
2025-10-04 16:57:38 -0300

    feat: add create question, create room and get room questio ns

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,6 +1,6 @@
-import { reset, seed } from "drizzle-seed"
-import { db, sql } from "./connection.ts"
-import { schema } from "./schema/index.ts"
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
 await reset(db, schema)
 
@@ -12,8 +12,11 @@ await seed(db, schema).refine((f) => ({
       description: f.loremIpsum(),
     },
   },
+  questions: {
+    count: 20,
+  },
 }))
 
 await sql.end()
 
-console.log("Database seeded")
+//console.log("Database seeded")
