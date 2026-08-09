a896fcfc10888ff19e63680f1593a9c1d7921e7e
Renato Nascimento <renatocorreianascimento@gmail.com>
2026-08-09 15:17:27 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,36 @@
-import { fastifyCors } from '@fastify/cors'
-import fastifyMultipart from '@fastify/multipart'
-import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors";
+import fastifyMultipart from "@fastify/multipart";
+import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod'
-import { env } from './env.ts'
-import { createQuestionRoute } from './http/routes/create-question.ts'
-import { createRoomsRoute } from './http/routes/create-room.ts'
-import { getRoomsQuestions } from './http/routes/get-room-questions.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
-import { uploadAudioRoute } from './http/routes/upload-audio.ts'
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
+import { createRoomsRoute } from "./http/routes/create-room.ts";
+import { getRoomsQuestions } from "./http/routes/get-room-questions.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
-})
+  origin: "http://localhost:5173",
+});
 
-app.register(fastifyMultipart)
+app.register(fastifyMultipart);
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.get('/health', () => 'OK')
+app.get("/health", () => "OK");
 
-app.register(getRoomsRoute)
-app.register(createRoomsRoute)
-app.register(getRoomsQuestions)
-app.register(createQuestionRoute)
-app.register(uploadAudioRoute)
+app.register(getRoomsRoute);
 
-app.listen({ port: env.PORT })
+app.register(createRoomsRoute);
+app.register(getRoomsQuestions);
+app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
+
+app.listen({ port: env.PORT });
